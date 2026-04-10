8fd09d2d1d0341262812e3000967345076276352
Mari Ozuna Garcia <111534971+mozunagarcia@users.noreply.github.com>
2026-04-10 16:50:43 -0700

    Hw finished

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,1 +1,18 @@
 // Put your code below
+module endianness_converter(
+    input clk,
+    input [31:0] word,
+    input endianness_in,
+    input endianness_out,
+    output reg [31:0] converted_word
+);
+
+always @(posedge clk)
+begin
+    if (endianness_in == endianness_out)
+        converted_word = word;
+    else
+        converted_word = {word[7:0], word[15:8], word[23:16], word[31:24]};
+end
+
+endmodule 
